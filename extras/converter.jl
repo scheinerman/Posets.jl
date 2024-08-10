f8f6e38614a14976693ae5bab3de0f6955bbd93c
Ed Scheinerman <ers@jhu.edu>
2024-08-10 13:27:11 -0400

    better poset_convert

--- extras/converter.jl
+++ extras/converter.jl
@@ -22,12 +22,13 @@ function poset_convert(p::Poset)::SimplePoset{Int}
 end
 
 
-function poset_convert(P::SimplePoset{T}, d::Dict{T,S})::Poset{Int} where {T,S<:Integer}
+
+function poset_convert(P::SimplePoset{T}, V::Vector{T})::Poset{Int} where {T}
     n = card(P)
     p = Poset(n)
 
     rel_list = SimplePosets.relations(P)
-    int_rel_list = [(d[a], d[b]) for (a, b) in rel_list]
+    int_rel_list = [(V[a], V[b]) for (a, b) in rel_list]
 
     Posets.add_relations!(p, int_rel_list)
     return p
@@ -35,23 +36,15 @@ end
 
 """
     poset_convert(P::SimplePoset{T})::Poset{Int} where {T}
-    poset_convert(P::SimplePoset{T}, d::Dict{T,S})::Poset{Int} where {T,S<:Integer}
+    poset_convert(P::SimplePoset{T}, V::Vector{T})::Poset{Int} where {T,S<:Integer}
 
 Convert a `SimplePoset` (from the `SimplePosets` module) to a `Poset` (from the `Posets` module).
-The dictionary `d` maps the vertex names in `P` to integers `1` through `n` (where `n` is the 
-number of elements in `P`). It's the user's responsibility to be sure that `d`'s keys are exactly 
-the `n` elements of `P` and the `d`'s values are exactly the integers `1` through `n`. If not, 
-bad things might happen. 
+The list `V` should contain the vertices of `P`.
 
-If `d` is omitted, then one will be created automatically.
+If `V` is omitted, the it is automatically set as `V = elements(P)`.
 """
 function poset_convert(P::SimplePoset{T})::Poset{Int} where {T}
     n = card(P)
-    d = Dict{T,Int}()
-    VP = elements(P)
-    for j in 1:n
-        d[VP[j]] = j
-    end
-
-    return poset_convert(P, d)
+    V = elements(P)
+    return poset_convert(P, V)
 end
