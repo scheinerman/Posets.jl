476f39b9474a258da1a376003966ebf1d2a1e396
Ed Scheinerman <ers@jhu.edu>
2024-08-29 16:33:24 -0400

    Better meet/join coding

--- src/meet-join.jl
+++ src/meet-join.jl
@@ -1,3 +1,84 @@
+"""
+    _lattice_op(p::Poset, x::Integer, y::Integer, join_flag::Bool)::Integer
+
+Determine the join or meet of `x` and `y` in `p`. Return `0` if not possible. 
+Return the join if `join_flag` is `true`. Otherwise, return the meet. 
+
+Common code for `lattice_join` and `lattice_meet`.
+"""
+function _lattice_op(p::Poset, x::Integer, y::Integer, join_flag::Bool)::Integer
+    n = nv(p)
+    if x < 1 || x > n || y < 1 || y > n   # not in the poset
+        return 0
+    end
+    if x == y
+        return x
+    end
+
+    if has_relation(p, x, y)
+        return join_flag ? y : x
+    end
+
+    # relatives are the set of elements above [below] both x and y
+    # provided join_flag is true [false].
+    relatives = Set{Int}()
+    if join_flag
+        relatives = Set{Int}(above(p, x)) ∩ Set{Int}(above(p, y))
+    else
+        relatives = Set{Int}(below(p, x) ∩ Set{Int}(below(p, y)))
+    end
+
+    if length(relatives) == 0
+        return 0
+    end
+
+    relatives = _chain_sort(p, collect(relatives))  # make a sorted list
+
+    # the join [meet] is the first element in relatives (if it exists)
+    z = 0
+    if join_flag
+        z = first(relatives)
+    else
+        z = last(relatives)
+    end
+
+    # check if z is below [above] all the other elements in relatives
+    nrels = length(relatives)
+    if join_flag
+        for i in 2:nrels
+            if !has_relation(p, z, relatives[i])
+                return 0
+            end
+        end
+    else
+        for i in 1:(nrels - 1)
+            if !has_relation(p, relatives[i], z)
+                return 0
+            end
+        end
+    end
+
+    return z
+end
+
+"""
+    lattice_join(p, x, y)
+
+Compute the join of `x` and `y` in `p`, or return `0` if it doesn't exist.
+"""
+lattice_join(p, x, y) = _lattice_op(p, x, y, true)
+
+"""
+    lattice_meet(p, x, y)
+
+Compute the join of `x` and `y` in `p`, or return `0` if it doesn't exist.
+
+"""
+lattice_meet(p, x, y) = _lattice_op(p, x, y, false)
+
+function _cannot_lattice_pq()
+    throw(ArgumentError("Cannot compute join/meet of elements in different posets"))
+end
 
 """
     vee(a::PosetElement, b::PosetElement)::PosetElement
@@ -8,41 +89,14 @@ Invoke like this: `p[x] ∨ p[y]`.
 function vee(a::PosetElement, b::PosetElement)::PosetElement
     p = a.p
     if p !== b.p   # different posets
-        _cannot_compare()
+        _cannot_lattice_pq()
     end
-
-    # special case
-    if a ≤ b
-        return b
+    x = integer(a)
+    y = integer(b)
+    z = _lattice_op(p, x, y, true)
+    if z == 0
+        throw(ArgumentError("$x ∨ $y does not exist in this poset"))
     end
-    if a ≥ b
-        return a
-    end
-
-    x = a.x
-    y = b.x
-    msg = "Elements $x and $y do not have a join in this poset"
-
-    # these are the elements strictly above x and y
-    overs = Set(above(p, x)) ∩ Set(above(p, y))
-
-    if length(overs) == 0
-        error(msg)
-    end
-
-    overlist = _chain_sort(p, collect(overs))
-
-    z = first(overlist)  # candidate for the join 
-
-    # check that z < every other element in overlist 
-    no = length(overlist)
-
-    for i in 2:no
-        if !has_relation(p, z, overlist[i])
-            error(msg)
-        end
-    end
-
     return p[z]
 end
 
@@ -55,40 +109,37 @@ Invoke like this: `p[x] ∧ p[y]`.
 function wedge(a::PosetElement, b::PosetElement)::PosetElement
     p = a.p
     if p !== b.p   # different posets
-        _cannot_compare()
+        _cannot_lattice_pq()
     end
-
-    # special case
-    if a ≤ b
-        return a
+    x = integer(a)
+    y = integer(b)
+    z = _lattice_op(p, x, y, false)
+    if z == 0
+        throw(ArgumentError("$x ∧ $y does not exist in this poset"))
     end
-    if a ≥ b
-        return b
-    end
+    return p[z]
+end
 
-    x = a.x
-    y = b.x
-    msg = "Elements $x and $y do not have a join in this poset"
-
-    # these are the elements strictly above x and y
-    unders = Set(below(p, x)) ∩ Set(below(p, y))
-
-    if length(unders) == 0
-        error(msg)
-    end
-
-    underlist = _chain_sort(p, collect(unders))
-
-    z = last(underlist)  # candidate for the join 
-
-    # check that z > every other element in underlist 
-    no = length(underlist)
-
-    for i in 1:(no - 1)
-        if !has_relation(p, underlist[i], z)
-            error(msg)
+function join_table(p)
+    n = nv(p)
+    T = zeros(Int, n, n)
+    for a in 1:n
+        for b in a:n
+            T[a, b] = lattice_join(p, a, b)
+            T[b, a] = T[a, b]
         end
     end
+    return T
+end
 
-    return p[z]
+function meet_table(p)
+    n = nv(p)
+    T = zeros(Int, n, n)
+    for a in 1:n
+        for b in a:n
+            T[a, b] = lattice_meet(p, a, b)
+            T[b, a] = T[a, b]
+        end
+    end
+    return T
 end
